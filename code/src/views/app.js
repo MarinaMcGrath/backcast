09849ca07e9d6aeff8095a0f4e1abfb020f6cbe2
Cain <cainwatson@users.noreply.github.com>
2017-07-10 17:41:21 -0500

    some stuff passin

--- code/src/views/app.js
+++ code/src/views/app.js
@@ -2,12 +2,16 @@ var AppView = Backbone.View.extend({
 
   el: '#app',
 
-  initialize: function() {
-    this.videos = new Videos();
+  initialize: function(videos) {
+    this.videos = new Videos(videos);
+    this.render();
   },
 
   render: function() {
     this.$el.html(this.template());
+    new VideoPlayerView();
+    new SearchView();
+    new VideoListView();
     return this;
   },
 
