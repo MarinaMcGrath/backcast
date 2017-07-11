c16ab57bb6005e8d8e4b647ac23e7a31cf36c08c
MarinaMcGrath <marinamcgrath92@gmail.com>
2017-07-11 11:07:16 -0500

    Add custom search feature

--- code/src/views/videoPlayer.js
+++ code/src/views/videoPlayer.js
@@ -1,15 +1,14 @@
 var VideoPlayerView = Backbone.View.extend({
   initialize: function(video) {
     this.video = video;
-    if (video === undefined) {
-      this.video = { attributes: null };
-    }
     this.render();
   },
 
   render: function() {
-    this.$el.html(this.template(this.video.attributes));
-    $('.player').html(this.$el);
+    if (this.video !== undefined) {
+      this.$el.html(this.template(this.video.attributes));
+      $('.player').html(this.$el);
+    }  
     return this;
   },
 
