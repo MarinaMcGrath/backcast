4ce728590bad70898580c1633ec97f2d9b007c42
Cain <cainwatson@users.noreply.github.com>
2017-07-10 21:08:35 -0500

    APIS OGOGLE! YOUTUBE

--- code/src/collections/videos.js
+++ code/src/collections/videos.js
@@ -1,9 +1,20 @@
+const query = 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&videoDefinition=high&videoEmbeddable=true&maxResults=5&key=';
+// q=javascript|backbonejs&
 var Videos = Backbone.Collection.extend({
   model: Video,
-  search: function() {
-    $.ajax({
-      url: 'https://www.googleapis.com/youtube/v3/search',
-      type: 'GET'
+  search: function(q) {
+    Backbone.ajax({
+      url: `${query}${window.YOUTUBE_API_KEY}`,
+      type: 'GET',
+      dataType: 'json',
+      data: { q: q },
+      success: function(data) {
+        console.log(data);
+        window.fetched = new Videos().parse(data);
+      }
     });
+  },
+  parse: function(data) {
+    return data.items;
   }
 });
--- code/src/views/videoPlayer.js
+++ code/src/views/videoPlayer.js
@@ -1,11 +1,13 @@
 var VideoPlayerView = Backbone.View.extend({
   initialize: function(video) {
     this.video = video;
+    if (video === undefined) {
+      this.video = { attributes: null }
+    }
     this.render();
   },
 
   render: function() {
-    console.log(this.video.attributes);
     this.$el.html(this.template(this.video.attributes));
     $('.player').html(this.$el);
     return this;
